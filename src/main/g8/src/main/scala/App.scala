fa69408d96c0abdf5f973fb42193dabb23f7ba13
Samuel Cox <sacox@paypal.com>
2016-10-13 10:02:29 -0500

    Show json4s usage

--- src/main/g8/src/main/scala/App.scala
+++ src/main/g8/src/main/scala/App.scala
@@ -2,17 +2,29 @@ import org.apache.http.client.fluent.{
   Form, Request
 }
 
+import org.json4s._
+import org.json4s.native.JsonMethods._
+
 object App {
+
+  case class Resource(a: Option[Int])
+
   def main(args: Array[String]): Unit = {
+
+    /*
     val content = 
       Request.Get("http://www.cnn.com").execute().returnContent()
 
     println(content)
+    */
 
-    /*
-    Request.Post("http://targethost/login")
-      .bodyForm(Form.form().add("username",  "vip").add("password",  "secret").build())
-      .execute().returnContent()
-      */
+    implicit val formats = DefaultFormats
+
+    val someJsonPayload = """[ { "a": 1 }, { "a": 2 }, { "b": 3 } ]"""
+
+    val resources = parse(someJsonPayload).extract[List[Resource]]
+
+    resources.foreach { println }
+
   }
 }
